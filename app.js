40737e33be9dc40204707379bc5ca3485590255d
Balogun <sodiqb86@gmail.com>
2021-07-13 11:15:10 +1300

    better

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ app.post('/paystack',urlencodedParser, (req,res)=>{
     
         });
     
-app.get('/metering-infrastructure.herokuapp.com/paystack/callback', (req,res) => {
+app.get('https://metering-infrastructure.herokuapp.com/paystack/callback', (req,res) => {
     const ref = req.query.reference;
     verifyPayment(ref, (error,body)=>{
         if(error){
